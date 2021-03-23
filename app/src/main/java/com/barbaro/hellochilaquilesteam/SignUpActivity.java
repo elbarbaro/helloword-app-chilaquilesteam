06571c6a460799f149bb94fbc36bf74b995b3d11
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2021-03-23 17:38:44 -0600

    New design for layouts

--- app/src/main/java/com/barbaro/hellochilaquilesteam/SignUpActivity.java
+++ app/src/main/java/com/barbaro/hellochilaquilesteam/SignUpActivity.java
@@ -2,7 +2,9 @@ package com.barbaro.hellochilaquilesteam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void navigateToMain(View view) {
+        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
