544623a89bba793f139a16bc1d621e09861288e5
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2021-04-16 11:56:15 -0500

    Navigate to main screen

--- app/src/main/java/com/barbaro/hellochilaquilesteam/SignUpActivity.java
+++ app/src/main/java/com/barbaro/hellochilaquilesteam/SignUpActivity.java
@@ -6,12 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import android.widget.EditText;
+import android.widget.CheckBox;
+import android.widget.Button;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextConfirmPassword;
+    private CheckBox checkBoxTerms;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
     }
 
     public void navigateToMain(View view) {
